3895069d8afbbbafa0eafc41b38802e8e2baba81
Tashus <alheadbme@gmail.com>
2019-03-24 22:41:38 -0500

    Slide down mountain

--- GameManager.cs
+++ GameManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject ground;
+	public GameObject playerObject;
+
+	float slope = 20f;
+
 	// Use this for initialization
 	void Start () {
-		
+		ground.transform.Rotate(slope, 0f, 0f, Space.World);
+		playerObject.transform.Rotate(slope, 0f, 0f, Space.World);
 	}
 	
 	// Update is called once per frame
--- Player.cs
+++ Player.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public GameObject cam;
+	Vector3 planeNorm = new Vector3();
+	GameObject skis;
+	float speed = 5f;
+	//float accell
+
 	// Use this for initialization
 	void Start () {
-		
+		skis = transform.Find("Skis").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		cam.transform.Translate(transform.position - cam.transform.position, Space.World);
 		
+	}
+
+	void FixedUpdate() {
+		float dt = Time.deltaTime;
+		transform.Translate(transform.forward * speed * dt, Space.World);
 	}
 }
