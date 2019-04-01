799992189ff6ef3c06e489e821d0c3ea206ef47f
Austin <austin.hilberg@gmail.com>
2019-04-01 12:01:19 -0500

    Add non-functional gates.

--- LevelManager.cs
+++ LevelManager.cs
@@ -7,6 +7,7 @@ public class LevelManager : MonoBehaviour {
 	float slope;
 
 	public GameObject tree;
+	public GameObject gate;
 
 	// Use this for initialization
 	void Start () {
@@ -21,11 +22,16 @@ public class LevelManager : MonoBehaviour {
 	public void GenerateLevel (float slope) {
 		this.slope = slope;
 
-		AddIntervalObject (tree, 25, 0f, 0f, 10f, 10f);
+		AddIntervalObject (gate, 10, -2f, 0f, 20f, 30f, false);
+		AddIntervalObject (gate, 10, 2f, 0f, 35f, 30f, true);
+		gate.SetActive(false);
+		AddIntervalObject(tree, 60, -10f, 0f, 5f, 5f, false);
+		AddIntervalObject(tree, 60, 10f, 0f, 5f, 5f, false);
 		tree.SetActive(false);
+
 	}
 
-	void AddIntervalObject (GameObject gob, int n, float x, float y, float zStart, float zInterval) {
+	void AddIntervalObject (GameObject gob, int n, float x, float y, float zStart, float zInterval, bool flip) {
 		Vector3 position = Vector3.zero;
 		float angle = slope * Mathf.Deg2Rad;
 		float sinAngle = Mathf.Sin(angle);
@@ -33,7 +39,7 @@ public class LevelManager : MonoBehaviour {
 		for (int i = 0; i < n; i ++) {
 			float zFlat = zStart + (zInterval * i);
 			position.Set(x, - zFlat * sinAngle, zFlat * cosAngle);
-			Instantiate(gob, position, Quaternion.Euler(0f, 0f, 0f));
+			Instantiate(gob, position, Quaternion.Euler(0f, 0f, flip ? 180f : 0f));
 		}
 	}
 }
